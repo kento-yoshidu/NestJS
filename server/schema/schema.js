05ede20a16ad1f6eb44a22e2d6ed38a5cd7e4a8f
kento <kento@toriwatari.bird>
2021-03-21 12:44:37 +0900

    Update schema.js

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -30,6 +30,28 @@ const RootQuery = new GraphQLObjectType({
   }
 })
 
+const Mutation = new GraphQLObjectType({
+  name: 'Mutation',
+  fields: {
+    addMovie: {
+      type: MovieType,
+      args: {
+        name: { type: GraphQLString},
+        genre: { type: GraphQLString},
+      },
+      resolve(parent, args) {
+        let movie = new Movie({
+          name: args.name,
+          genre: args.genre
+        })
+        return movie.save();
+      }
+    }
+  }
+})
+
 module.exports = new GraphQLSchema({
-  query: RootQuery
+  query: RootQuery,
+  mutation: Mutation,
+  allData: AllData
 })
